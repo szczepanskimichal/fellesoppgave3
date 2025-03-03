eafe9f4c9a64d0532a34f284d0b2f35eeb661647
SanderK36 <sander.k606@gmail.com>
2025-03-03 13:55:46 +0100

    d

---
+++ script.js
@@ -0,0 +1,14 @@
+const events = ["You met a friend along the way!",
+            "A monster appeared on the road!",
+            "The car behind you is mad, you got into a road rage!",];
+
+updateView();
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/`
+    <h1>Random Event Test</h1>  
+    `;
+}
+
+function randomEvent() {
+    
+}
