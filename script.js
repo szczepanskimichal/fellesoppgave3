c42f03490f052f6b7c26aeec2f67edba063efb2b
SanderK36 <sander.k606@gmail.com>
2025-03-03 14:19:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const events = ["You met a friend along the way!",
-            "A monster appeared on the road!",
-            "The car behind you is mad, you got into a road rage!",];
+const events = [
+    {
+        description: "You met a friend along the way!",
+        choices: [
+            { text: "Offer a ride", response: "Your friend says, 'Thanks!' and hops in." },
+            { text: "Wave and drive on", response: "Your friend waves back but looks a bit angry." },
+            { text: "Stop to chat", response: "You talk for a bit, then keep driving." }
+        ]
+    },
+    {
+        description: "A monster appeared on the road!",
+        choices: [
+            { text: "Swerve around it", response: "You barely miss the monster and speed away!" },
+            { text: "Fight it", response: "You stop and scare it off with your horn." },
+            { text: "Scream and panic", response: "The monster laughs and runs off." }
+        ]
+    },
+    {
+        description: "The car behind you is mad, you got into a road rage!",
+        choices: [
+            { text: "Speed up", response: "You lose them in traffic!" },
+            { text: "Slow down", response: "They pass you, yelling out the window." },
+            { text: "Honk back", response: "It turns into a honking war, but they eventually leave." }
+        ]
+    }
+];
+
+const currentMessage = "You are driving down the road.";
+const currentChoices = [];
+const isEventActive = false;
 
 updateView();
+setTimeout(randomEvent, 5000);
+
 function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/`
     <h1>Random Event Test</h1>  
+    <div id="message">${currentMessage}</div>
     `;
 }
 
